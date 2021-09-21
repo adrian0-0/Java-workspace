6014aee1d91fd3e2f5858c194395d1cd04482d2b
Adrian Vinícius Roma Alves <75907345+adrian0-0@users.noreply.github.com>
2021-09-21 18:17:26 -0300

    21/08 +update

--- POB_Aula08/src/Renda_Futebol.java
+++ POB_Aula08/src/Renda_Futebol.java
@@ -1,4 +1,18 @@
 
 public class Renda_Futebol {
+	
+	static void Calculo_Renda() {
+		String time_mandante;
+		String time_visitante;
+		float renda_mandante;
+		float renda_visitante;
+	}
+	
+	public static void main(String[]args) {
+		Calculo_Renda futebol = new Calculo_Renda();
+		
+		futebol.time_mandante = "oi";
+		futebol.time_visi
+	}
 
 }
